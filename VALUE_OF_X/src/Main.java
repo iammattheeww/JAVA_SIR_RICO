b34afb5f6d795b88b57f24ac1373c56d35b3d205
Matthew Justin Intoy <2402438@cclab.usls.edu>
2026-08-13 10:10:55 +0800

    Converted VALUE_OF_X from submodule to standard folder

---
+++ VALUE_OF_X/src/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        String x = "Hello";
+        String y = "World";
+        System.out.println("The value of x is " + x + " and the value of y is " + y);
+    }
+}
