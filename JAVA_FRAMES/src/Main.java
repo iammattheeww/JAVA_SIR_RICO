dafd72e695c0083c7f150a964487023644f54417
Justin <bhorbhor2014@gmail.com>
2026-08-18 06:09:23 +0800

    Added Java Frames snippet

---
+++ JAVA_FRAMES/src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main{
+    public static void main(String[] xyz){
+        Frame myFrame = new Frame();
+        myFrame.setSize(1024, 768);
+        myFrame.setLayout(null);
+        myFrame.setVisible(true);
+    }
+}
